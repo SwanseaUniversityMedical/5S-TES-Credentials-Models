6ef730f0432c4e66bb0b48203b9f406173e745c2
Rowan Dash <rowan.dash@swansea.ac.uk>
2026-02-04 10:56:27 +0000

    fix: add ""

--- .github/commitlint.config.mjs
+++ .github/commitlint.config.mjs
@@ -8,6 +8,7 @@ export default {
       RuleConfigSeverity.Error,
       "always",
       [
+        "",
         "GraphQLRunner-container",
         "SQLRunnerPostgres-container",
         "SQLRunnerTrino-container",
